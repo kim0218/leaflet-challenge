556856ba47b56772105bdc2341a7f819e84fc124
kim hogan <kimhogan7495@gmail.com>
2021-04-24 14:37:11 -0400

    commit again

--- static/js/logic.js
+++ static/js/logic.js
@@ -42,6 +42,28 @@ d3.json(link).then(function(data) {
                 opacity: 1,
                 fillOpacity: 0.8
             })
-        }
+        },
+
+        
       }).addTo(myMap);
+
+      var legend = L.control({position: 'bottomright'});
+
+      legend.onAdd = function (map) {
+
+          var div = L.DomUtil.create('div', 'info legend'),
+              grades = [10,30,50,70,90],
+              labels = [];
+
+          // loop through our density intervals and generate a label with a colored square for each interval
+          for (var i = 0; i < grades.length; i++) {
+              div.innerHTML +=
+                  '<i style="background:' + getcolor(grades[i] + 1) + '"></i> ' +
+                  grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+          }
+
+          return div;
+      };
+
+      legend.addTo(myMap);
     });   
