d8a37a8ff9eedb3eba3f96129932f59810bc6768
kim hogan <kimhogan7495@gmail.com>
2021-04-24 09:44:22 -0400

    commit final

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoia2ltaG9nYW43NDk1IiwiYSI6ImNrbmdseW83ZTB2bnYydnFxdXZ4bWl1ZnIifQ.YhoYNuar0dC8xStZKYj7eg";
---
+++ static/js/logic.js
@@ -0,0 +1,47 @@
+var myMap = L.map("mapid", {
+    center: [40.7128, -74.0059],
+    zoom: 5
+  });
+
+  // Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+  // Use this link to get the geojson data.
+  var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+  function getcolor(i){
+    return i > 90 ? "#EA2C2C" :
+        i > 70  ? "#EA822C" :
+        i > 50  ? "#EE9C00" :
+        i > 30  ? "#EECC00" :
+        i > 10   ?  "#D4EE00" :
+                "#98EE00";
+      }
+  
+ // Grabbing our GeoJSON data..
+d3.json(link).then(function(data) {
+    // Creating a geoJSON layer with the retrieved data
+    L.geoJson(data, { 
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h1>" + feature.properties.mag + "</h1> <hr> <h2>" + feature.properties.place + "</h2>");
+
+        },
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: feature.properties.mag*4,
+                fillColor: getcolor(feature.geometry.coordinates[2]),
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            })
+        }
+      }).addTo(myMap);
+    });   
